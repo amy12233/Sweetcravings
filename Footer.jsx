b5648e1c40f20bafb0a8db2ba5211697fe20b5b0
amy <93597699+amy12233@users.noreply.github.com>
2024-12-02 14:59:55 -0600

    Create Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 Sweet Cravings By Victoria. All rights reserved.</p>
+    </footer>
+  );
+}
+
